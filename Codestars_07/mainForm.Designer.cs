4ca9c6f4e407c1d4306ff5b15ccc442ba3bbf6e5
Владимир <omelin.vladimir@gmail.com>
2015-02-11 21:34:06 +0300

    7 задача

---
+++ Codestars_07/mainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Codestars_07
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbPercents = new System.Windows.Forms.TextBox();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "result";
+            this.openFileDialog.Filter = "Data files|*.data|All files|*.*";
+            this.openFileDialog.Title = "Выберите файл с зашифрованным хитом";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip.TabIndex = 2;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьФайлToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьФайлToolStripMenuItem
+            // 
+            this.открытьФайлToolStripMenuItem.Name = "открытьФайлToolStripMenuItem";
+            this.открытьФайлToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.открытьФайлToolStripMenuItem.Text = "Открыть файл...";
+            this.открытьФайлToolStripMenuItem.Click += new System.EventHandler(this.открытьФайлToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // tbPercents
+            // 
+            this.tbPercents.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbPercents.Location = new System.Drawing.Point(0, 24);
+            this.tbPercents.Multiline = true;
+            this.tbPercents.Name = "tbPercents";
+            this.tbPercents.ReadOnly = true;
+            this.tbPercents.Size = new System.Drawing.Size(284, 237);
+            this.tbPercents.TabIndex = 3;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tbPercents);
+            this.Controls.Add(this.menuStrip);
+            this.Name = "mainForm";
+            this.Text = "Насколько она твоя";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьФайлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbPercents;
+    }
+}
+
---
+++ Codestars_07/mainForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Codestars_07
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            var file = File.OpenText(openFileDialog.FileName);
+            string line;
+            char[] starPart = { 'a', 'b', 'c', 'd' };
+            char[] myPart = { 'w', 'x', 'y', 'z' };
+            int[] maxs = { 0, 0 };
+            int[] currents = { 0, 0 };
+            bool isMyPart = false;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                foreach (var ch in line)
+                {
+                    if (isMyPart)
+                    {
+                        if (myPart.Contains(ch)) currents[1]++;
+                        if (starPart.Contains(ch))
+                        {
+                            if (maxs[1] < currents[1]) maxs[1] = currents[1];
+                            isMyPart = false;
+                            currents[0]++;
+                            currents[1] = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (myPart.Contains(ch))
+                        {
+                            if (maxs[0] < currents[0]) maxs[0] = currents[0];
+                            isMyPart = true;
+                            currents[1]++;
+                            currents[0] = 0;
+                        }
+                        if (starPart.Contains(ch)) currents[0]++;
+                    }
+                }
+            }
+
+            file.Close();
+
+            if (maxs[0] < currents[0]) maxs[0] = currents[0];
+            if (maxs[1] < currents[1]) maxs[1] = currents[1];
+
+            tbPercents.Text = ((float)maxs[1] / (float)(maxs[0] + maxs[1]) * 100).ToString() + "%";
+        }
+    }
+}
