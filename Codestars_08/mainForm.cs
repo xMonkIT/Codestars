ed0871aef8a2aef8c411a2da8a689f022ab6790c
Владимир <omelin.vladimir@gmail.com>
2015-02-11 21:58:03 +0300

    8 задача

---
+++ Codestars_08/mainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Codestars_03
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            var file = File.OpenText(openFileDialog.FileName);
+            var languages = new List<HashSet<int>>();
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                var fairies = new HashSet<int>();
+                foreach (var fairy in line.Split(' ')) fairies.Add(int.Parse(fairy));
+                var currentLanguages = new List<HashSet<int>>();
+                foreach (var fairy in fairies)
+                    foreach (var language in languages)
+                        if (language.Contains(fairy))
+                            currentLanguages.Add(language);
+                if (currentLanguages.Count == 0) languages.Add(fairies);
+                else
+                {
+                    for (int i = 1; i < currentLanguages.Count; i++)
+                    {
+                        if (currentLanguages[0] != currentLanguages[1])
+                        {
+                            currentLanguages[0].UnionWith(currentLanguages[i]);
+                            languages.Remove(currentLanguages[i]);
+                        }
+                    }
+                    foreach (var fairy in fairies)
+                        if (!currentLanguages[0].Contains(fairy)) currentLanguages[0].Add(fairy);
+                }
+            }
+
+            int max = 0;
+
+            foreach (var language in languages)
+                if (language.Count > max) max = language.Count;
+
+            nudCount.Value = max;
+            file.Close();
+        }
+    }
+}
